ba57aa70b23f1afdc764e421e16f3d093320e161
azu <azuciao@gmail.com>
2018-02-20 01:43:45 +0900

    update

--- dangerfile.js
+++ dangerfile.js
@@ -6,9 +6,8 @@ const TRAVIS_TOKEN = process.env.TRAVIS_TOKEN;
 
 fetchFailureJobs(TRAVIS_BUILD_ID, TRAVIS_TOKEN).then(jobList => {
     if (jobList.length > 0) {
-        warn(`Some optional test is failed. See [CI](https://travis-ci.org/azu/danger-test/builds/${TRAVIS_BUILD_ID})`);
+        warn(`${jobList.length} optional test is failed. Check [Travis CI Build Results](https://travis-ci.org/azu/danger-test/builds/${TRAVIS_BUILD_ID})`);
     } else {
         message("Success All!");
     }
 });
-message("You have added 2 more modules to the app")
